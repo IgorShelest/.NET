b47c522902c496a9dee883c8ddfc57a7580fc7e4
Шелест Ігор <shelest.igor@icloud.com>
2020-02-28 13:03:51 +0200

    Added 'InitializationController' which simulates sending of initial data to UI layer

---
+++ UIArchitecturePatterns/WebAPIApplications/WebAPIApplication/Controllers/InitializationController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebAPIApplication.Dtos;
+
+namespace WebAPIApplication.Controllers
+{
+    [RoutePrefix("api/initialization")]
+    public class InitializationController : ApiController
+    {
+        //---------------------------------------------------------------------
+
+        [HttpGet]
+        public IHttpActionResult GetInitializationData()
+        {
+            var dto = new InitializationDataDto();
+
+            return Ok(dto);
+        }
+
+        //---------------------------------------------------------------------
+    }
+}
---
+++ UIArchitecturePatterns/WebAPIApplications/WebAPIApplication/Dtos/InitializationDataDto.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WebAPIApplication.Enums;
+
+namespace WebAPIApplication.Dtos
+{
+    public class InitializationDataDto
+    {
+        //---------------------------------------------------------------------
+
+        public Dictionary<int, string> TransactionStatusMappings = new Dictionary<int, string>
+        {
+            { (int) CurrencyCode.UnknownCurrency, CurrencyCode.UnknownCurrency.ToString() },
+            { (int) CurrencyCode.USD, CurrencyCode.USD.ToString() },
+            { (int) CurrencyCode.JPY, CurrencyCode.JPY.ToString() },
+            { (int) CurrencyCode.THB, CurrencyCode.THB.ToString() },
+            { (int) CurrencyCode.SGD, CurrencyCode.SGD.ToString() }
+        };
+
+        //---------------------------------------------------------------------
+
+        public Dictionary<int, string> CurrencyCodeMappings = new Dictionary<int, string>
+        {
+            { (int)TransactionStatus.UnknownStatus, TransactionStatus.UnknownStatus.ToString() },
+            { (int)TransactionStatus.Success, TransactionStatus.Success.ToString() },
+            { (int)TransactionStatus.Failed, TransactionStatus.Failed.ToString() },
+            { (int)TransactionStatus.Cancelled, TransactionStatus.Cancelled.ToString() }
+        };
+
+        //---------------------------------------------------------------------
+    }
+}
